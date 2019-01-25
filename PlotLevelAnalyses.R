f60d6d33264255fdef778fb6bf367806f5cd173b
Jane M Cowles <cowl0037@ad.umn.edu>
2019-01-25 17:26:33 -0600

    Beginning Plot Level Analyses

---
+++ PlotLevelAnalyses.R
@@ -0,0 +1,73 @@
+### Plot level ML for species ID, etc
+#### Jane M Cowles
+
+library(hsdar)
+library(caTools)
+library(rgdal)
+library(raster)
+library(spatstat)
+library(rgeos)
+library(maptools)
+library(sf)
+library(ggmap)
+library(PBSmapping)
+### general packages that I use often
+library(plyr)
+library(nlme)
+library(lme4)
+library(car)
+library(ggplot2)
+library(vegan)
+library(readxl)
+library(data.table)
+library(tidyr)
+library(gridExtra)
+library(RColorBrewer)
+library(latticeExtra)
+
+
+#for parallel processing
+library(parallel)
+
+#for making a beep noise
+library(beepr)
+
+
+##### I can update this later with Sys.info()[['sysname']] -- the biggest hurdle is the st_write where you need to include the shapefile name as part of the dsn in windows but not in mac.
+
+# #directories for MY MAC
+# computer <- 'mac'
+# RemoteSenDataLoc <- "/Volumes/HyperDrive/Google Drive/remote sensing data/"
+# LocalSource <- "~/Dropbox/UMN Postdoc/Ortho_Proc/" #imu and biocon shapefile (polygons)
+# ProcLoc <- "/Volumes/HyperDrive/JaneProc/"
+# VisLoc <- "/Volumes/HyperDrive/BioCon....."
+#directories for ISBELL PC
+computer <- 'pc'
+RemoteSenDataLoc <- "F:/RemoteSensing/"
+LocalSource <- "~/Ortho_Proc/" #imu and biocon shapefile (polygons)
+ProcLoc <- "F:/JaneProc/"
+VisLoc <- "F:/BioCON10Aug--VISUAL/"
+
+plot.files <- list.files(ProcLoc,pattern=".*Plot.*shp")
+
+system.time(POI <- readOGR(paste0(ProcLoc,plot.files[2])))
+plot(POI)
+spplot(POI)
+
+POI_DF <- as.data.frame(POI)
+colnames(POI_DF)<-gsub("_",".",colnames(POI_DF))
+colnames(POI_DF)<-substr(colnames(POI_DF),3,9)
+
+setDT(POI_DF)
+
+POI_DFsub <- POI_DF[,c(1:272,276,277)]
+POI_DFsub$Unique <- 1:nrow(POI_DFsub)
+POI_DFL<- melt(POI_DFsub,id=c("Lat2","Lon2","Unique"))
+POI_DFL$nm <- as.numeric(substr(POI_DFL$variable,3,9))
+
+plot(value~nm,POI_DFL)
+
+ggplot(POI_DFL,aes(nm,value,group=Unique,color=Unique))+geom_line()
+
+# POI_DFL<- melt(POI_DF,patterns=c("nm"),id=c("Lat2","Lon2"))
+str(POI_DFL)
