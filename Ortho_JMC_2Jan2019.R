6c5f93084e1d5687b7d8a49c9b5d9a17b5fe49d7
Jane M Cowles <cowl0037@ad.umn.edu>
2019-01-26 13:35:07 -0600

    add plot buffer

--- Ortho_JMC_2Jan2019.R
+++ Ortho_JMC_2Jan2019.R
@@ -47,11 +47,7 @@ LocalSource <- "~/Ortho_Proc/" #imu and biocon shapefile (polygons)
 ProcLoc <- "F:/JaneProc/"
 VisLoc <- "F:/BioCON10Aug--VISUAL/"
 
-# fakefunction <- function(x){
-#   print(x)
-#   print(computer)
-#   }
-# fakefunction("will the computer know what I set as computer outside of the loop???")
+
 
 
 no_cores <- detectCores()-1
@@ -69,7 +65,7 @@ imusp <- spTransform(imusp,"+init=epsg:32615")
 imu <- as.data.frame(imusp[,!names(imusp) %in% c("Lon","Lat")])
 latMinIMU <- imu$Lat[imu$Alt==min(imu$Alt)]
 lonMinIMU <- imu$Lon[imu$Alt==min(imu$Alt)]
-rm(imu)#keep things tidy
+rm(imu)
 rm(imuxy)
 rm(imusp)
 imu.framematch <- read.csv(paste0(LocalSource,"FramexIMU.csv"))
@@ -165,7 +161,7 @@ imuTR <- as.data.frame(sp.tmpTRfordem[,!(names(sp.tmpTRfordem)%in%c("Lon","Lat")
   tmp$PitchCorrFactor <-  PitchCorrFactor
   tmp$YawCorrFactor <-  YawCorrFactor
   
-  # TESTERS
+  # TESTING (ignore)
   # tmp<-data.frame(nrow=1)
   # tmp$Roll <- 1
   # tmp$Pitch <- 1
@@ -175,7 +171,7 @@ imuTR <- as.data.frame(sp.tmpTRfordem[,!(names(sp.tmpTRfordem)%in%c("Lon","Lat")
   
   #ROLL -- Negative sign added 1/3/2019 in order to account for the reversal of what happens to the drone and which way the sensor points!
    tmp$Rolloffset <- -tmp$HeightAboveGround*tan(tmp$Roll) #parallel to the frame's long direction (as specified by yaw), therefore:
-   tmp$RollYComponent <- -tmp$Rolloffset*sin(tmp$Yaw) ###(FOR NOW REMOVING THIS NEGATIVE SIGN TO SEE HOW THINGS LOOK!!!!!)#### PUTTING IT BACK IN
+   tmp$RollYComponent <- -tmp$Rolloffset*sin(tmp$Yaw) ###(FOR NOW REMOVING THIS NEGATIVE SIGN TO SEE HOW THINGS LOOK!!!!!)#### THAT WAS WRONG PUTTING IT BACK IN
    tmp$RollXComponent <- tmp$Rolloffset*cos(tmp$Yaw)
    
   #PITCH
@@ -221,10 +217,8 @@ return(out)
 #note I have min(imu$Alt) -- only works because I have the full imu loaded (instead of the imu file of just the positions with images taken)
 Proc_IMU <- imu_proc(imu.datafile = imu.framematch,GroundLevel=overallIMUmin,FOVAngle = 15.9619, degree=T,coords.epsg=4326,minAlt_dem_atminIMU=minAlt_dem_atminIMU,dem_rast=dem_rast)
 
-# rm(imu.framematch)
 
 
-# write.csv(Proc_IMU,paste0(LocalSource,"Proc_10Jan2019.csv"),row.names = F)
 
 
 spacing_fun <- function(pix_i,matchimu,specdfframe){
@@ -232,7 +226,7 @@ spacing_fun <- function(pix_i,matchimu,specdfframe){
   
   ########### THINK ABOUT THIS
   ########### Do I need to flip the + and - here to get these in the right direction. Should it be +Y and -X?????
-  ########### I ask because the images are just perfectly flipped -- gasp!
+  ########### (because the images are just perfectly flipped...so, yes they do)
   
   
   #Original!!!!
@@ -243,10 +237,7 @@ spacing_fun <- function(pix_i,matchimu,specdfframe){
   specdfpix$Lat2<-matchimu$Lat+matchimu$TotMidPointCorrectionY-matchimu$YawYOffset+(pix_i*2*matchimu$YawYOffset/640)
   specdfpix$Lon2<-matchimu$Lon+matchimu$TotMidPointCorrectionX+matchimu$YawXOffset-(pix_i*2*matchimu$YawXOffset/640)
   specdfpix$Heading <- matchimu$Heading
-  
-  
-  ####new attempt -- minus the midpoint correction, too!
-  
+
   return(specdfpix)
 }
 
@@ -260,64 +251,37 @@ centerandends_corr <- function(framex,spectral.data.frame,ProcessedIMU){
 
 shpfile_plotloop <-function(plotnum,specdfOUT_sf,PlotShapeFile,filenumber,computer=computer,ProcLoc=ProcLoc){
   plottmp <- subset(PlotShapeFile,PLOTID==plotnum)
-  plot_sf <- st_as_sf(plottmp)
+  # plot_sf <- st_as_sf(plottmp)
   #for now, I want to test if I can see the plot edges as a square within the buffered region
-  # plot_buff <- gBuffer(subset(PlotShapeFile,PLOTID==plotnum),width = 0.5)
-  # plot_sf <- st_as_sf(plot_buff)
+  plot_buff <- gBuffer(plottmp,width = -0.5)
+  plot_sf <- st_as_sf(plot_buff)
   st_crs(plot_sf)<-st_crs(specdfOUT_sf)
   suppressWarnings(plot_clip <- st_intersection(specdfOUT_sf,plot_sf))
 
   if(dim(plot_clip)[1]>0){
     print(plotnum)
     if(computer=="mac"){
-  st_write(plot_clip,dsn=paste0(ProcLoc),layer=paste0("BUFFProc",filenumber,"Plot",plotnum),driver="ESRI Shapefile",delete_layer=TRUE)}
+  st_write(plot_clip,dsn=paste0(ProcLoc),layer=paste0("CorrProcBUFF",filenumber,"Plot",plotnum),driver="ESRI Shapefile",delete_layer=TRUE)}
 
   if(computer=="pc"){
-    st_write(plot_clip,dsn=paste0(ProcLoc,"BUFFProc",filenumber,"Plot",plotnum,".shp"),layer=paste0("BUFFProc",filenumber,"Plot",plotnum),driver="ESRI Shapefile",delete_layer=TRUE)}
+    st_write(plot_clip,dsn=paste0(ProcLoc,"CorrProcBUFF",filenumber,"Plot",plotnum,".shp"),layer=paste0("CorrProcBUFF",filenumber,"Plot",plotnum),driver="ESRI Shapefile",delete_layer=TRUE)}
   }
 }
 
 #######################################################################
 #############to run smaller test runs
-ProcessedIMU=Proc_IMU
-PlotShapeFile=plotshp
 
-filenumber <- 34317
+subset_fun <- function(filenumber,framesofinterest){
 orig_sp <-readGDAL(paste0(RemoteSenDataLoc,"20180917/100040_bc_2018_09_17_14_48_50/raw_",filenumber))
 spectral.data.frame <- as.data.frame(orig_sp)
 orig_sp <- NULL
 colnames(spectral.data.frame)[1:272]<-paste0("nm",bandtowave$Wavelength)
 spectral.data.frame$frame <- filenumber+max(spectral.data.frame$y)-spectral.data.frame$y
-sub34317 <- spectral.data.frame[spectral.data.frame$frame%in%c(35000:35500),]
+subtmp <- spectral.data.frame[spectral.data.frame$frame%in%c(framesofinterest),]
 
-filenumber <- 2816
-orig_sp <-readGDAL(paste0(RemoteSenDataLoc,"20180917/100040_bc_2018_09_17_14_48_50/raw_",filenumber))
-spectral.data.frame <- as.data.frame(orig_sp)
-orig_sp <- NULL
-colnames(spectral.data.frame)[1:272]<-paste0("nm",bandtowave$Wavelength)
-spectral.data.frame$frame <- filenumber+max(spectral.data.frame$y)-spectral.data.frame$y
-sub2816 <- spectral.data.frame[spectral.data.frame$frame%in%c(3100:3600),]
 rm(spectral.data.frame)
-
-filenumber <- 9200
-orig_sp <-readGDAL(paste0(RemoteSenDataLoc,"20180917/100040_bc_2018_09_17_14_48_50/raw_",filenumber))
-spectral.data.frame <- as.data.frame(orig_sp)
-orig_sp <- NULL
-colnames(spectral.data.frame)[1:272]<-paste0("nm",bandtowave$Wavelength)
-spectral.data.frame$frame <- filenumber+max(spectral.data.frame$y)-spectral.data.frame$y
-sub9200 <- spectral.data.frame[spectral.data.frame$frame%in%c(10650:11150),]
-rm(spectral.data.frame)
-
-
-filenumber <- 1024
-orig_sp <-readGDAL(paste0(RemoteSenDataLoc,"20180917/100040_bc_2018_09_17_14_48_50/raw_",filenumber))
-spectral.data.frame <- as.data.frame(orig_sp)
-orig_sp <- NULL
-colnames(spectral.data.frame)[1:272]<-paste0("nm",bandtowave$Wavelength)
-spectral.data.frame$frame <- filenumber+max(spectral.data.frame$y)-spectral.data.frame$y
-sub1024 <- spectral.data.frame[spectral.data.frame$frame%in%c(2200:2700),]
-rm(spectral.data.frame)
-
+return(subtmp)
+}
 
 ortho_funSUB <- function(subdataframe,ProcessedIMU,PlotShapeFile,bandtowave,framesofinterest){
   spectral.data.frame<-subdataframe
@@ -355,89 +319,35 @@ ortho_funSUB <- function(subdataframe,ProcessedIMU,PlotShapeFile,bandtowave,fram
   
 }
 
+sub2816 <- subset_fun(filenumber=2816,framesofinterest=3100:3600)
 
 Proc_IMU <- imu_proc(imu.datafile = imu.framematch,GroundLevel=overallIMUmin,FOVAngle = 15.9619, degree=T,coords.epsg=4326,minAlt_dem_atminIMU=minAlt_dem_atminIMU,dem_rast=dem_rast)
 
+system.time(rast_2816<-ortho_funSUB(sub2816,ProcessedIMU=Proc_IMU,PlotShapeFile=plotshp,bandtowave=bandtowave,framesofinterest = c(8000:8500)));beep(2)
 
-Proc_IMUMULTICorr <- imu_proc(imu.datafile = imu.framematch,GroundLevel=overallIMUmin,FOVAngle = 15.9619, degree=T,coords.epsg=4326,minAlt_dem_atminIMU=minAlt_dem_atminIMU,dem_rast=dem_rast,YawCorrFactor = 0.4, RollCorrFactor = 0.01,PitchCorrFactor = -0.04)
+Proc_IMUMULTICorr <- imu_proc(imu.datafile = imu.framematch,GroundLevel=overallIMUmin,FOVAngle = 15.9619, degree=T,coords.epsg=4326,minAlt_dem_atminIMU=minAlt_dem_atminIMU,dem_rast=dem_rast,YawCorrFactor = .45, RollCorrFactor = -0.075,PitchCorrFactor = 0.005)
 
-# system.time(rast_3100<-ortho_funSUB(sub2816,ProcessedIMU=Proc_IMU,PlotShapeFile=plotshp,bandtowave=bandtowave,framesofinterest = c(3100:3600)));beep(2)
-# 
-# system.time(rast_3100MULTI<-ortho_funSUB(sub2816,ProcessedIMU=Proc_IMUMULTICorr,PlotShapeFile=plotshp,bandtowave=bandtowave,framesofinterest = c(3100:3600)));beep(2)
-# 
-# plot(ring2rel)
-# plot(rast_3100MULTI,breaks=breakpoints,col=c(mycol,"blue","darkblue"),add=T)
-# plot(plotshp,add=T)
-
-# system.time(rast_35000<-ortho_funSUB(sub34317,ProcessedIMU=Proc_IMU,PlotShapeFile=plotshp,bandtowave=bandtowave,framesofinterest = c(35000:35500)));beep(2)
-# system.time(rast_35000MULTI<-ortho_funSUB(sub34317,ProcessedIMU=Proc_IMUMULTICorr,PlotShapeFile=plotshp,bandtowave=bandtowave,framesofinterest = c(35000:35500)));beep(2)
-# 
-# plot(ring2rel)
-# plot(rast_3100MULTI,breaks=breakpoints,col=c(mycol,"blue","darkblue"),add=T)
+system.time(rast_2816MULTI<-ortho_funSUB(sub2816,ProcessedIMU=Proc_IMUMULTICorr,PlotShapeFile=plotshp,bandtowave=bandtowave,framesofinterest = c(8000:8500)));beep(2)
 
 
-# plot(ring2vis)
-# ring2rel <- crop(ring2vis,extent(rast_3100)+5)
-
-# 
-# breakpoints <- c(minValue(rast_3100),minValue(rast_3100)+150,minValue(rast_3100)+250,maxValue(rast_3100))
-# mycol <- rgb(0, 0, 255, max = 255, alpha = 5, names = "blue50")
-# plot(rast_3100,breaks=breakpoints,col=c(mycol,"yellow","red"),add=T)
 
 
-system.time(rast_1024<-ortho_funSUB(sub1024,ProcessedIMU=Proc_IMU,PlotShapeFile=plotshp,bandtowave=bandtowave,framesofinterest = c(2200:2700)));beep(2)
-system.time(rast_1024MULTI<-ortho_funSUB(sub1024,ProcessedIMU=Proc_IMUMULTICorr,PlotShapeFile=plotshp,bandtowave=bandtowave,framesofinterest = c(2200:2700)));beep(2)
+
 
 # plot(ring1vis)
-# ring2rel <- crop(ring2vis,extent(rast_1024MULTI)+5)
-breakpoints <- c(minValue(rast_1024),minValue(rast_1024)+150,minValue(rast_1024)+250,maxValue(rast_1024))
+# ring2rel <- crop(ring2vis,extent(rast_2816)+5)
+breakpoints <- c(minValue(rast_2816),minValue(rast_2816)+150,minValue(rast_2816)+250,maxValue(rast_2816))
 mycol <- rgb(0, 0, 255, max = 255, alpha = 5, names = "blue50")
 
 plot(ring2rel)
-plot(rast_1024,breaks=breakpoints,col=c(mycol,"yellow","red"),add=T)
+plot(rast_2816,breaks=breakpoints,col=c(mycol,"yellow","red"),add=T)
 
-plot(rast_1024MULTI,breaks=breakpoints,col=c(mycol,"blue","darkblue"),add=T)
+plot(rast_2816MULTI,breaks=breakpoints,col=c(mycol,"blue","darkblue"),add=T)
 
-
-n# plot(ring2vis)
-# ring2rel <- crop(ring2vis,extent(rast_3100)+5)
-
-
-
-
-# plot(ring2vis)
-# ring6rel <- crop(ring6vis,extent(rast_35000)+5)
-
-plot(ring6rel)
-
-breakpoints <- c(minValue(rast_35000),minValue(rast_35000)+150,minValue(rast_35000)+250,maxValue(rast_35000))
-mycol <- rgb(0, 0, 255, max = 255, alpha = 5, names = "blue50")
-plot(rast_35000,breaks=breakpoints,col=c(mycol,"yellow","red"),add=T)
-plot(rast_35000MULTI,breaks=breakpoints,col=c(mycol,"blue","darkblue"),add=T)
 plot(plotshp,add=T)
 
 
 
-plot(ring2rel)
-plot(rast_3100,breaks=breakpoints,col=c(mycol,"yellow","red"),add=T)
-plot(rast_3100YAW,breaks=breakpoints,col=c(mycol,"white","darkblue"),add=T)
-plot(ring2rel)
-plot(rast_3100,breaks=breakpoints,col=c(mycol,"yellow","red"),add=T)
-plot(rast_3100PITCH,breaks=breakpoints,col=c(mycol,"black","grey"),add=T)
-plot(ring2rel)
-plot(rast_3100,breaks=breakpoints,col=c(mycol,"yellow","red"),add=T)
-plot(rast_3100ROLL,breaks=breakpoints,col=c(mycol,"white","grey"),add=T)
-
-plot(rast_3100,breaks=breakpoints,col=colors)
-# plot(rast_3100,add=T,col=c("red","orange"))
-plot(plotshp,add=T)
-plot(proc_3100,add=T)
-spplot(ring2vis)+as.layer(spplot(rast_3100))
-spplot(rast_3100)+as.layer(spplot(ring2vis),under=TRUE)
-
-
-plot(subset(plotshp,RingID==2))
-spplot(rast_3100)+as.layer(subset(plotshp,RingID==2))
 
 ##############################################end of test area
 ##############################################
@@ -454,18 +364,12 @@ ortho_fun <- function(filenumber,ProcessedIMU,PlotShapeFile,bandtowave){
   colnames(spectral.data.frame)[1:272]<-paste0("nm",bandtowave$Wavelength)
   
   #### here is where I want to FLIP IT!!!!
-  ###flipped version: (NEXT: Do i want this - 0.5 anymore??)
+  ###flipped version -- is correct.
     spectral.data.frame$frame <- filenumber+max(spectral.data.frame$y)-spectral.data.frame$y
 ### original version
   # spectral.data.frame$frame <- filenumber+spectral.data.frame$y-0.5
     
     
-    ###FOR SUBSET TESTS
-    # spectral.data.frame <- sub34317
-    # filenumber <- 25000
-    # spectral.data.frame <- sub2816
-    # filenumber <- 3100
-    #run each of these with different corrections on roll,    
     spectral.data.frame$Lat2 <- NA
     spectral.data.frame$Lon2 <- NA
     spectral.data.frame$Heading <- NA
@@ -487,13 +391,12 @@ ortho_fun <- function(filenumber,ProcessedIMU,PlotShapeFile,bandtowave){
 
 plotshp <- spTransform(plotshp,proj4string(specdfOUT_sp))
 
-#commenting out for faster test runs
-# cl2<-makeCluster(no_cores)
-# clusterExport(cl2,c("st_crs","st_crs<-","subset","st_as_sf","st_intersection","st_write"),envir=environment())
-# parLapply(cl2,sort(unique(plotshp$PLOTID)),shpfile_plotloop,specdfOUT_sf,PlotShapeFile,filenumber,computer,ProcLoc)
-# stopCluster(cl2)
+#comment this out for faster test runs
+cl2<-makeCluster(no_cores)
+clusterExport(cl2,c("st_crs","st_crs<-","subset","st_as_sf","st_intersection","st_write","gBuffer"),envir=environment())
+parLapply(cl2,sort(unique(plotshp$PLOTID)),shpfile_plotloop,specdfOUT_sf,PlotShapeFile,filenumber,computer,ProcLoc)
+stopCluster(cl2)
 
-### lapply(sort(unique(plotshp$PLOTID)),shpfile_plotloop,specdfOUT_sf,PlotShapeFile,filenumber)
 
   if(computer=="mac"){
     st_write(specdfOUT_sf,dsn=paste0(ProcLoc),layer=paste0("NEWProc",filenumber,"full"),driver="ESRI Shapefile",delete_layer=TRUE)
@@ -501,15 +404,15 @@ plotshp <- spTransform(plotshp,proj4string(specdfOUT_sp))
     print(filenumber) }else{print("notmac!")}
   
   if(computer=="pc"){
-    st_write(specdfOUT_sf,dsn=paste0(ProcLoc,"NEWProcRollandYawCorr_",filenumber,"full.shp"),layer=paste0("Proc_RollandYawCorr",filenumber,"full"),driver="ESRI Shapefile",delete_layer=TRUE)
+    st_write(specdfOUT_sf,dsn=paste0(ProcLoc,"CorrFin_",filenumber,"full.shp"),layer=paste0("CorrFin",filenumber,"full"),driver="ESRI Shapefile",delete_layer=TRUE)
     print(Sys.time())
     print(filenumber)}else{print("notpc!")}
 
 #commenting out for faster test runs
-# means_out <- over(plotshp,specdfOUT_sp,fn=mean)
-# means_out$Plot <- 1:nrow(means_out)
-# means_out$File <- filenumber
-# return(means_out)
+means_out <- over(plotshp,specdfOUT_sp,fn=mean)
+means_out$Plot <- 1:nrow(means_out)
+means_out$File <- filenumber
+return(means_out)
   
 
 }
@@ -517,10 +420,12 @@ plotshp <- spTransform(plotshp,proj4string(specdfOUT_sp))
 
 listoffilenums <- sort(unique(as.numeric(gsub("\\D", "",list.files(paste0(RemoteSenDataLoc,"20180917/100040_bc_2018_09_17_14_48_50/"))))))
 
+Proc_IMU2816 <- imu_proc(imu.datafile = imu.framematch,GroundLevel=overallIMUmin,FOVAngle = 15.9619, degree=T,coords.epsg=4326,minAlt_dem_atminIMU=minAlt_dem_atminIMU,dem_rast=dem_rast,YawCorrFactor = .45, RollCorrFactor = -0.075,PitchCorrFactor = 0.005);system.time(out_df2816 <- rbindlist(lapply(listoffilenums[c(3)],ortho_fun,ProcessedIMU=Proc_IMU2816,PlotShapeFile=plotshp,bandtowave=bandtowave)));beep(3)
 
-system.time(lapply(listoffilenums[c(2)],ortho_fun,ProcessedIMU=Proc_IMUMULTICorr,PlotShapeFile=plotshp,bandtowave=bandtowave));beep(2)
+Proc_IMU9200 <- imu_proc(imu.datafile = imu.framematch,GroundLevel=overallIMUmin,FOVAngle = 15.9619, degree=T,coords.epsg=4326,minAlt_dem_atminIMU=minAlt_dem_atminIMU,dem_rast=dem_rast,YawCorrFactor = 0.16, RollCorrFactor = 0.01,PitchCorrFactor = -0.04);system.time(out_df9200 <- rbindlist(lapply(listoffilenums[c(9)],ortho_fun,ProcessedIMU=Proc_IMU9200,PlotShapeFile=plotshp,bandtowave=bandtowave)));beep(2)
 
-system.time(out_df3 <- rbindlist(lapply(listoffilenums[c(3)],ortho_fun,ProcessedIMU=Proc_IMUMULTICorr,PlotShapeFile=plotshp,bandtowave=bandtowave)));beep(2)
-system.time(out_df5 <- rbindlist(lapply(listoffilenums[c(5)],ortho_fun,ProcessedIMU=Proc_IMU,PlotShapeFile=plotshp,bandtowave=bandtowave)));beep(2)
-system.time(out_df25 <- rbindlist(lapply(listoffilenums[c(25)],ortho_fun,ProcessedIMU=Proc_IMU,PlotShapeFile=plotshp,bandtowave=bandtowave)));beep(2)
+Proc_IMU1024 <- imu_proc(imu.datafile = imu.framematch,GroundLevel=overallIMUmin,FOVAngle = 15.9619, degree=T,coords.epsg=4326,minAlt_dem_atminIMU=minAlt_dem_atminIMU,dem_rast=dem_rast,YawCorrFactor = 0.4, RollCorrFactor = 0.01,PitchCorrFactor = -0.04);system.time(out_df1024 <- rbindlist(lapply(listoffilenums[c(2)],ortho_fun,ProcessedIMU=Proc_IMU1024,PlotShapeFile=plotshp,bandtowave=bandtowave)));beep(2)
+
+system.time(lapply(listoffilenums[c(7)],ortho_fun,ProcessedIMU=Proc_IMUMULTICorr,PlotShapeFile=plotshp,bandtowave=bandtowave));beep(2)
+
 
